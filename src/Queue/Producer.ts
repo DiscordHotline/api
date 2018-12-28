0b485d850a83a693d2f0718e3c2a484eac2413b9
Aaron Scherer <aequasi@gmail.com>
2018-12-28 13:51:39 -0800

    Adding auto-recovery to queue

--- src/Queue/Producer.ts
+++ src/Queue/Producer.ts
@@ -1,11 +1,12 @@
-import {Channel, connect, Connection, Replies} from 'amqplib';
+import * as withAutoRecovery from 'amqplib-auto-recovery';
+import * as amqp from 'amqplib/callback_api';
+import {Channel, Connection, Replies} from 'amqplib/callback_api';
 import {inject, injectable, postConstruct} from 'inversify';
+import {Logger} from 'winston';
 import Types from '../types';
 
 @injectable()
 export default class Queue {
-    private connection: Connection;
-
     private channel: Channel;
 
     private exchange: Replies.AssertExchange;
@@ -22,15 +23,33 @@ export default class Queue {
 
     @postConstruct()
     public async initialize(): Promise<void> {
-        this.connection = await connect({
-            hostname:  this.host,
-            port:      parseInt(this.port, 10),
-            username:  this.username,
-            password:  this.password,
-            vhost:     'hotline',
-            heartbeat: 5,
+        return new Promise(async (resolve, reject) => {
+            await withAutoRecovery(amqp).connect(
+                {
+                    hostname:  this.host,
+                    port:      parseInt(this.port, 10),
+                    username:  this.username,
+                    password:  this.password,
+                    vhost:     'hotline',
+                    heartbeat: 5,
+                },
+                (err, conn) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    conn.createChannel((e, channel) => {
+                        if (e) {
+                            return reject(e);
+                        }
+
+                        this.channel = channel;
+                        resolve();
+                    });
+                },
+            );
         });
-        this.channel    = await this.connection.createChannel();
+
     }
 
     public async publish(message: any): Promise<boolean> {
