7c6a3d578bcbfca329824ab81f9bf41b9d00e849
Aaron Scherer <aequasi@gmail.com>
2018-12-28 09:49:49 -0800

    Adding lambda warmpup

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ module.exports.handler = async (event, context) => {
     if (!handler) {
         handler = await Kernel();
     }
+    if (event.source === 'serverless-plugin-warmup') {
+        console.log('WarmUP - Lambda is warm!');
+
+        return {
+            statusCode: 200,
+            body:       'Lambda is warm!',
+        };
+    }
 
     return serverless(handler)(event, context);
 };
